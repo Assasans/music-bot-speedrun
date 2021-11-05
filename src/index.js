0efa653dddf133d724430c49fd489ac0beae965a
Daniil Pryima <swimmin2@gmail.com>
2021-11-05 22:28:08 +0200

    Use yt-dlp only to extract stream URL

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const Fs = require('promise-fs');
 const Path = require('path');
 
 const { PassThrough } = require('stream');
-const { spawn } = require('child_process');
+const { spawn, exec } = require('child_process');
 const { Client, Intents } = require('discord.js');
 const {
 	joinVoiceChannel,
@@ -14,12 +14,24 @@ const {
 	VoiceConnectionStatus
 } = require('@discordjs/voice');
 
+function execAsync(command) {
+	return new Promise((resolve, reject) => {
+		exec(command, (error, stdout, stderr) => {
+			if(error) return reject(error);
+			return resolve({ stdout: stdout, stderr: stderr });
+		});
+	})
+}
+
 (async () => {
 	const config = JSON.parse(await Fs.readFile(Path.resolve('config.json'), { encoding: 'utf8' }));
 
 	const client = new Client({
 		intents: [
-			new Intents(641)
+			Intents.FLAGS.GUILDS,
+			Intents.FLAGS.GUILD_MEMBERS,
+			Intents.FLAGS.GUILD_MESSAGES,
+			Intents.FLAGS.GUILD_VOICE_STATES
 		]
 	});
 
@@ -32,7 +44,9 @@ const {
 	client.on('messageCreate', async (message) => {
 		const guild = message.guild;
 		const content = message.content;
+		const member = message.member;
 
+		if(!member) return;
 		if(!content.startsWith('%')) return;
 
 		const rawArgs = content.slice(1).split(' ');
@@ -42,7 +56,7 @@ const {
 		if(command === 'play') {
 			const url = args[0].replace('<', '').replace('>', '');
 
-			const channel = message.member.voice.channel;
+			const channel = member.voice.channel;
 			if(!channel) {
 				message.reply('You are not in a voice channel');
 				return;
@@ -72,64 +86,63 @@ const {
 
 			// const stream = new PassThrough();
 
-			const youtubeProcess = spawn('youtube-dl', [
-				encodeURI(url),
-				'--cookies', Path.resolve('cookies.txt'),
-				'--format', '251/91',
-				'-o', '-'
-			], {
-				stdio: 'pipe'
-			});
+			// let youtubeProcess;
+			let ffmpegProcess;
+			// for(let i = 0; i < 60; i++) {
+				const streams = (await execAsync(`yt-dlp ${encodeURI(url)} --cookies ${Path.resolve('cookies.txt')} --get-url`)).stdout.toString().split('\n');
+				const streamUrl = streams[0].endsWith('.m3u8') ? streams[0] : streams[1];
 
-			const ffmpegProcess = spawn('ffmpeg', [
-				'-hide_banner',
-				'-loglevel', 'panic',
-				'-i', 'pipe:0',
-				'-ac', '2',
-				'-f', 's16le',
-				'-ar', '48000',
-				'-b:a', '131072',
-				'pipe:1'
-			], {
-				stdio: 'pipe'
-			});
+				ffmpegProcess = spawn('ffmpeg', [
+					'-hide_banner',
+					'-i', streamUrl,
+					'-loglevel', 'quiet',
+					'-f', 's16le',
+					'-ar', '48000',
+					'-ac', '2',
+					'-b:a', '384k',
+					'-threads', '4',
+					'pipe:1'
+				], {
+					stdio: 'pipe'
+				});
 
-			ffmpegProcess.stderr.on('data', (chunk) => {
-				console.log(chunk.toString());
-			});
+				ffmpegProcess.stderr.on('data', (chunk) => {
+					console.log(chunk.toString());
+				});
 
-			// ffmpegProcess.stdout.on('data', (chunk) => {
+				// ffmpegProcess.stdout.on('data', (chunk) => {
+				// 	const buffer = Buffer.from(chunk);
+
+				// 	// console.log(chunk.toString());
+				// 	stream.write(buffer);
+				// 	// console.log(`[ffmpeg -> discord.js] Written ${buffer.byteLength} bytes`);
+				// });
+
+				// ffmpegProcess.stdout.on('data', (chunk) => {
+
+				// });
+
+				// youtubeProcess.on('exit', (code) => {
+				// 	console.log(`[youtube-dl] Exited with exit code ${code}`)
+				// });
+
+				ffmpegProcess.on('exit', (code) => {
+					console.log(`[ffmpeg] Exited with exit code ${code}`)
+				});
+
+				// youtubeProcess.stderr.on('data', (chunk) => {
+				// 	console.log(chunk.toString());
+				// });
+			// }
+
+			// Pipe youtube-dl to ffmpeg
+			// youtubeProcess.stdout.on('data', (chunk) => {
 			// 	const buffer = Buffer.from(chunk);
 
 			// 	// console.log(chunk.toString());
-			// 	stream.write(buffer);
-			// 	// console.log(`[ffmpeg -> discord.js] Written ${buffer.byteLength} bytes`);
+			// 	ffmpegProcess.stdin.write(buffer);
+			// 	// console.log(`[youtube-dl -> ffmpeg] Written ${buffer.byteLength} bytes`);
 			// });
-
-			// ffmpegProcess.stdout.on('data', (chunk) => {
-
-			// });
-
-			youtubeProcess.on('exit', (code) => {
-				console.log(`[youtube-dl] Exited with exit code ${code}`)
-			});
-
-			ffmpegProcess.on('exit', (code) => {
-				console.log(`[ffmpeg] Exited with exit code ${code}`)
-			});
-
-			youtubeProcess.stderr.on('data', (chunk) => {
-				console.log(chunk.toString());
-			});
-
-			// Pipe youtube-dl to ffmpeg
-			youtubeProcess.stdout.on('data', (chunk) => {
-				const buffer = Buffer.from(chunk);
-
-				// console.log(chunk.toString());
-				ffmpegProcess.stdin.write(buffer);
-				// console.log(`[youtube-dl -> ffmpeg] Written ${buffer.byteLength} bytes`);
-			});
 
 			player.on('error', console.error);
 			player.on('debug', console.log);
